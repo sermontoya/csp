ff21d44d6b5c0f766ddbaf33abdcf623ff4084f5
Leister Alvarado <leister.alvarado@gmail.com>
2026-04-20 22:06:46 -0600

    Update init to consider latest functions

--- cps_backtracking/csp/__init__.py
+++ cps_backtracking/csp/__init__.py
@@ -1,3 +1,25 @@
-from .csp import Course, backtracking, initialize, is_consistent
+from .csp import (
+    Course,
+    ac3,
+    backtracking,
+    backtracking_with_inference,
+    initialize,
+    is_consistent,
+    revise,
+    select_degree,
+    select_mrv,
+    select_mrv_degree,
+)
 
-__all__ = ["Course", "initialize", "is_consistent", "backtracking"]
+__all__ = [
+    "Course",
+    "initialize",
+    "is_consistent",
+    "backtracking",
+    "backtracking_with_inference",
+    "select_mrv",
+    "select_degree",
+    "select_mrv_degree",
+    "revise",
+    "ac3",
+]
